15ee6bf0c02330f7fc574315bb4c6cfec41b1e1d
RobotLeopard86 <owensiebers@icloud.com>
2023-09-09 21:02:29 -0700

    Update environment setup script

--- setupEnvironment.sh
+++ setupEnvironment.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
-echo "Setting up Citrus Engine dev environment..."
-
-echo "Fetching Git changes..."
-git submodule update --recursive
-git pull
+if ! [ -f "$(which wget)" ]
+then
+    echo "This script requires wget!"
+fi
 
 echo "Setting up boost..."
 mkdir ./tmp
-wget -O ./tmp/boost.tar.bz2 https://boostorg.jfrog.io/artifactory/main/release/1.82.0/source/boost_1_82_0.tar.bz2
+echo "Downloading boost archive..."
+wget -q --show-progress -O ./tmp/boost.tar.bz2 https://boostorg.jfrog.io/artifactory/main/release/1.82.0/source/boost_1_82_0.tar.bz2 && printf '\e[A\e[K'
+echo "Unpacking boost archive..."
 tar --bzip2 -xf ./tmp/boost.tar.bz2 -C ./tmp
 cwd=$(pwd)
 mv ./tmp/boost_1_82_0 ./tmp/boost
 cd ./tmp/boost
+echo "Setting up boost build environment..."
 ./bootstrap.sh --with-libraries=test,thread,system --prefix="$cwd/libs/boost"
+echo "Installing boost library to Citrus Engine libraries directory..."
 ./b2 install
 cd $cwd
 
