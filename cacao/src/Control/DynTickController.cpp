dbb73a97878c8985231b72e2c2da5f165d4859f3
RobotLeopard86 <owensiebers@icloud.com>
2024-02-26 20:56:20 -0800

    Got the script discovery working
    This will fail when run, no world loading system is in place yet

--- cacao/src/Control/DynTickController.cpp
+++ cacao/src/Control/DynTickController.cpp
@@ -3,8 +3,10 @@
 #include "Core/Log.hpp"
 #include "Core/Engine.hpp"
 #include "Utilities/MiscUtils.hpp"
+#include "World/WorldManager.hpp"
+#include "Scripts/Script.hpp"
 
-#include <sstream>
+#include <typeinfo>
 
 namespace Cacao {
 	//Required static variable initialization
@@ -57,6 +59,41 @@ namespace Cacao {
 			std::chrono::steady_clock::time_point tickStart = std::chrono::steady_clock::now();
 			std::chrono::steady_clock::time_point idealStopTime = tickStart + std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::seconds(1)/Engine::GetInstance()->cfg.targetDynTPS);
 			
+			//Find all scripts that need to be run
+			std::vector<std::reference_wrapper<Script>> scriptsToRun;
+			World& activeWorld = WorldManager::GetInstance()->AccessWorld(WorldManager::GetInstance()->GetActiveWorld());
+			BS::multi_future<void> fsFuture = Engine::GetInstance()->GetThreadPool().submit_loop<unsigned int>(0, activeWorld.worldTree.children.size(), [scriptsToRun, activeWorld](unsigned int index) {
+				//Create script locator function for an entity
+				auto scriptLocator = [scriptsToRun](TreeItem<Entity>& e) {
+					//Sneaky recursive lambda trick
+					auto impl = [scriptsToRun](TreeItem<Entity>& e, auto& implRef) mutable {
+						//Stop if this component is inactive
+						if(!e.val().active) return;
+
+						//Check for script components
+						for(Component& c : e.val().components){
+							if(typeid(c) == typeid(Script&) && c.IsActive()) {
+								//Cast to script and add to list
+								Script& asScript = static_cast<Script&>(c);
+								scriptsToRun.push_back(asScript);
+							}
+						}
+
+						//Recurse through children
+						for(TreeItem<Entity>& child : e.children){
+							implRef(child, implRef);
+						}
+					};
+					return impl(e, impl);
+				};
+
+				//Execute the script locator
+				TreeItem<Entity>& ent = const_cast<TreeItem<Entity>&>(activeWorld.worldTree.children.at(index)); //We have to use const_cast because at() returns a const reference
+				scriptLocator(ent);
+			});
+			//Wait for work to be completed
+			fsFuture.wait();
+
 			//Check elapsed time
 			std::chrono::steady_clock::time_point tickEnd = std::chrono::steady_clock::now();
 			
