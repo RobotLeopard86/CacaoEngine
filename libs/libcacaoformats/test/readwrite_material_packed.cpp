07cd3cfba54510624f85bf497a2b7d0d3e555111
RobotLeopard86 <owensiebers@gmail.com>
2025-03-04 22:05:14 -0800

    Test renaming

--- libs/libcacaoformats/test/readwrite_material_packed.cpp
+++ libs/libcacaoformats/test/readwrite_material_packed.cpp
@@ -8,13 +8,13 @@ int main() {
 		//Write it
 		{
 			libcacaoformats::Material mat;
-			mat.shader = "yappucino";
-			mat.keys.insert_or_assign("test", libcacaoformats::Vec3<int> {.x = 7, .y = -4, .z = 3});
+			mat.shader = "aShader";
+			mat.keys.insert_or_assign("test_vec", libcacaoformats::Vec3<int> {.x = 7, .y = -4, .z = 3});
 			libcacaoformats::Matrix<float, 2, 3> trix;
 			trix[0] = {2.4f, 3.1f};
 			trix[1] = {66.1f, 9.143f};
 			trix[2] = {100.0f, 31.4f};
-			mat.keys.insert_or_assign("goose", trix);
+			mat.keys.insert_or_assign("test_mat", trix);
 			libcacaoformats::PackedEncoder enc;
 			std::ofstream str("./out.cacao", std::ios::binary);
 			enc.EncodeMaterial(mat).ExportToStream(str);
@@ -28,13 +28,13 @@ int main() {
 			libcacaoformats::PackedContainer container = libcacaoformats::PackedContainer::FromStream(str);
 			libcacaoformats::Material mat = dec.DecodeMaterial(container);
 			str.close();
-			if(mat.shader.compare("yappucino") != 0) throw std::runtime_error("Wrong shader reference!");
+			if(mat.shader.compare("aShader") != 0) throw std::runtime_error("Wrong shader reference!");
 			if(mat.keys.size() != 2) throw std::runtime_error("Wrong amount of keys!");
-			if(!mat.keys.contains("goose") || !mat.keys.contains("test")) throw std::runtime_error("Wrong keys decoded!");
-			libcacaoformats::Vec3<int> test = std::get<4>(mat.keys.at("test"));
-			libcacaoformats::Matrix<float, 2, 3> goose = std::get<13>(mat.keys.at("goose"));
-			if(test.x != 7 || test.y != -4 || test.z != 3) throw std::runtime_error("\"test\" key contains wrong values!");
-			if(goose[0][0] != 2.4f || goose[0][1] != 3.1f || goose[1][0] != 66.1f || goose[1][1] != 9.143f || goose[2][0] != 100.0f || goose[2][1] != 31.4f) throw std::runtime_error("\"goose\" key contains wrong values!");
+			if(!mat.keys.contains("test_mat") || !mat.keys.contains("test_vec")) throw std::runtime_error("Wrong keys decoded!");
+			libcacaoformats::Vec3<int> testVec = std::get<4>(mat.keys.at("test_vec"));
+			libcacaoformats::Matrix<float, 2, 3> testMat = std::get<13>(mat.keys.at("test_mat"));
+			if(testVec.x != 7 || testVec.y != -4 || testVec.z != 3) throw std::runtime_error("\"testVec\" key contains wrong values!");
+			if(testMat[0][0] != 2.4f || testMat[0][1] != 3.1f || testMat[1][0] != 66.1f || testMat[1][1] != 9.143f || testMat[2][0] != 100.0f || testMat[2][1] != 31.4f) throw std::runtime_error("\"testMat\" key contains wrong values!");
 		}
 
 		return 0;
