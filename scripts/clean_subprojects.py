880f35c675e3990fce5af4dfed8f06830edbb464
RobotLeopard86 <owensiebers@gmail.com>
2025-10-23 21:33:11 -0700

    Try to fix Windows subproject cleaner issues

--- scripts/clean_subprojects.py
+++ scripts/clean_subprojects.py
@@ -4,6 +4,7 @@ import pathlib
 import glob
 import os
 import shutil
+import stat
 
 # Directories
 basedir = pathlib.Path(sys.argv[0]).parent.parent
@@ -11,8 +12,8 @@ subdir = basedir / 'subprojects'
 
 # Read gitignore
 with open(basedir / '.gitignore') as gitignore:
-    global ignore
-    ignore = gitignore.readlines()
+	global ignore
+	ignore = gitignore.readlines()
 
 # Get a list of expressions to exclude
 sublines = list(filter(lambda x: 'subprojects' in x, ignore))
@@ -22,9 +23,9 @@ deletewraps = list(map(lambda y: pathlib.PurePath(y[:-1]).name, filter(lambda x:
 # Expand globs
 globbed = list(map(lambda x: glob.glob(pathlib.PurePath(x).name, root_dir=subdir, include_hidden=True), excludes))
 flattened = [
-    x
-    for y in globbed
-    for x in y
+	x
+	for y in globbed
+	for x in y
 ]
 keep = list(filter(lambda x: not x in deletewraps, flattened))
 
@@ -34,8 +35,14 @@ remove = list(set(subfiles).difference(set(keep)))
 
 # Remove
 for r in remove:
-    p = subdir / r
-    print('Removing', p)
-    shutil.rmtree(p) if p.is_dir() else os.remove(p)
+	p = subdir / r
+	print('Removing', p)
+	if p.is_dir():
+		for p2 in p.rglob('*'):
+			if p2.is_file():
+				p2.chmod(p.stat().st_mode | stat.S_IWRITE)
+		shutil.rmtree(p)
+	else:
+		os.remove(p)
 
 print('Complete.')
